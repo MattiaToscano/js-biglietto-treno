376789c6a98ea9c248c671c207e99acae5d2b4af
MattiaToscano <mattia1997ct@gmail.com>
2025-03-17 13:44:27 +0100

    [UPD] Fine esercizio

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ let prezzo = userChilometri * 0.21;
 
 if(userEtà <18){
     prezzo = prezzo - (prezzo * 0.2);
-    console.log('Verrà applicato lo sconto per minorenni ' + prezzo) 
+    console.log('Verrà applicato lo sconto per minorenni ' + prezzo.toFixed(2)); 
 }
 
 else if(userEtà >=65){
     prezzo = prezzo - (prezzo * 0.4);
-    console.log('Verrà applicato lo sconto per over 65 ' + prezzo) 
+    console.log('Verrà applicato lo sconto per over 65 ' + prezzo.toFixed(2));
 }
 
 else{prezzo = prezzo;
-    console.log('Nessuno sconto applicato ' + prezzo)
+    console.log('Nessuno sconto applicato ' + prezzo.toFixed(2));
 }
 
