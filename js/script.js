f76c91db9aef6109cfb428d063304ab6335bf016
MattiaToscano <mattia1997ct@gmail.com>
2025-03-17 13:40:16 +0100

    [UPD] Inserito ultimo else

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ let userEtà = prompt ('Inserisci età');
 let prezzo = userChilometri * 0.21;
 
 
+
 if(userEtà <18){
     prezzo = prezzo - (prezzo * 0.2);
     console.log('Verrà applicato lo sconto per minorenni ' + prezzo) 
 }
 
-else if(userEtà >65){
+else if(userEtà >=65){
     prezzo = prezzo - (prezzo * 0.4);
     console.log('Verrà applicato lo sconto per over 65 ' + prezzo) 
 }
 
-dd
+else{prezzo = prezzo;
+    console.log('Nessuno sconto applicato ' + prezzo)
+}
 
