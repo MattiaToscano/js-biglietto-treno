7a4fc2b10686ae5d11ba085f3be42136bb4774dd
MattiaToscano <mattia1997ct@gmail.com>
2025-03-17 13:46:57 +0100

    [UPD] Inseriti commenti procedura.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+
+
+
+// 1. Chiediamo all'utente quanti chilomentri vorrà percorrere.
 let userChilometri = prompt ('Inserisci chilometri Viaggio');
+
+// 2. Chiediamo all'utente l'età.
 let userEtà = prompt ('Inserisci età');
+
+//3. Calcoliamo il prezzo del biglietto in base ai chilometri inseriti dall'utente.
 let prezzo = userChilometri * 0.21;
 
 
-
+//4. Applichiamo uno sconto del 20% per i minorenni e del 40% per gli over 65.
 if(userEtà <18){
     prezzo = prezzo - (prezzo * 0.2);
     console.log('Verrà applicato lo sconto per minorenni ' + prezzo.toFixed(2)); 
